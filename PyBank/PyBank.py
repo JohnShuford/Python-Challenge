d32097e0cfe98619b1189099b36b01525cff4edb
Johnny Shuford <johnnyshuford@Johnnys-MacBook-Pro.local>
2020-09-23 12:39:43 -0600

    Finished the readout and stated working on the max and min for profits

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -10,15 +10,26 @@ with open ('budget_data.csv') as csv_file:
     line_count = 0
     month_count = 0
     total = 0
+    profits = []
+
+    #defining the buckets
+    great_in_mnth = ""
+    great_in_val = 0
+    great_dec_mnth = ""
+    great_dec_val = 0
+
     for (row) in csv_reader:
         line_count += 1
         if line_count > 1:
-            #making sure the months aren't the same then finding the count
-            if row[0] == row[0] + 1:
-                month_count = month_count + 1
+            #calculatint the month count
+            month_count += 1
             #calculating the total
             total  = total + int(row[1])
-    
+            #Adding the profits in to a list
+            profits.append(row[1])
+    print(profits)
+    great_in_val = max(profits)
+    great_dec_val = min(profits)
 
     #creating the readout
     print("Financial Analysis")
@@ -26,3 +37,5 @@ with open ('budget_data.csv') as csv_file:
     print(f"Total Months: {month_count}")
     print(f"Total: ${total}")
     print(f"Average Change: ${total/line_count}")
+    print(f"Greatest Increase in Profits: {great_in_val}")
+    print(f"Greatest Decrease in Prifits: {great_dec_val}")
